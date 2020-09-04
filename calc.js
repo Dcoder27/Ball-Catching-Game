14552596863d04be24037c2a7b5c5f8260cf595e
Dcoder27 <58599483+Dcoder27@users.noreply.github.com>
2020-09-04 19:10:11 +0530

    Update calc.js

--- calc.js
+++ calc.js
@@ -14,173 +14,202 @@ var DOMstrings = {
         equalButton: document.getElementById("equal_button"),
         scrollRow: document.getElementById("scrollTR"),
         scrollTable: document.getElementById("scrollData")
-};   
-    
+};
+
     var resultContent = [];
     var allEquations = [];
+    let equalPressed = false;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
- 
-function displayNumber(num, val) 
+
+function displayNumber(num, val)
 {
     resultContent.push(val);
     allEquations.push(val);
     
-
     
     if (document.getElementById("result").textContent === '0' || DOMstrings.resultDisplay.textContent === '')
     {
         DOMstrings.resultDisplay.textContent = val;
-    } else 
+    } else
     {
         DOMstrings.resultDisplay.textContent += val;
     }
-    
+
 }
 
 
-function resulting() 
+function resulting()
 {
     let final = resultContent.join('');
     let finalResult = eval(final);
     DOMstrings.resultDisplay.textContent = finalResult;
-    allEquations.push(' =\n' + finalResult)
+    allEquations.push(' =\n' + finalResult);
+}
+
+function scrollDisplay(value) {
+    
+  var tableRef = document.getElementById('scrollData').getElementsByTagName('tbody')[0];
+
+  // Insert a row in the table at row index 0
+  var newRow   = tableRef.insertRow(tableRef.rows.length);
+
+  // Insert a cell in the row at index 0
+  var newCell  = newRow.insertCell(0);
+
+  // Append a text node to the cell
+    
+  var newText  = document.createTextNode(value);
+  newCell.appendChild(newText); 
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
-        
+
          DOMstrings.number1.onclick = function (){
                 displayNumber(DOMstrings.number1, '1');
+                scrollDisplay('1');
             }
-         
+
 
      DOMstrings.number2.onclick = function() {
                 displayNumber(DOMstrings.number2, '2');
+                scrollDisplay('2');
             }
-     
+
 
      DOMstrings.number3.onclick = function() {
                 displayNumber(DOMstrings.number3, '3');
+                scrollDisplay('3');
             }
-     
+
 
      DOMstrings.number4.onclick = function() {
                 displayNumber(DOMstrings.number4, '4');
+                scrollDisplay('4');
             }
-     
+
 
      DOMstrings.number5.onclick = function() {
                 displayNumber(DOMstrings.number5, '5');
+                scrollDisplay('5');
             }
-     
-     
+
+
      DOMstrings.number6.onclick = function() {
                 displayNumber(DOMstrings.number6, '6');
+                scrollDisplay('6');
             }
-     
+
 
      DOMstrings.number7.onclick = function() {
                 displayNumber(DOMstrings.number7, '7');
+                scrollDisplay('7');
             }
-     
+
 
      DOMstrings.number8.onclick = function() {
                 displayNumber(DOMstrings.number8, '8');
+                scrollDisplay('8');
             }
-     
-     
+
+
      DOMstrings.number9.onclick = function() {
                 displayNumber(DOMstrings.number9, '9');
+                scrollDisplay('9');
             }
-     
-     
+
+
 
      DOMstrings.number0.onclick = function() {
                 displayNumber(DOMstrings.number0, '0');
+                scrollDisplay('0');
             }
-     
-////////////////////////////////////////////////////////////////////////////////////////////////////     
-     
-     
-    document.getElementById("decimal_button").onclick = function() 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+    document.getElementById("decimal_button").onclick = function()
     {
+        scrollDisplay('.');
         DOMstrings.resultDisplay.textContent += '.';
         resultContent.push('.');
         allEquations.push('.');
     }
-    
-    
-        document.getElementById("plus_button").onclick = function() 
+
+
+        document.getElementById("plus_button").onclick = function()
     {
+        scrollDisplay('+');
         resultContent.push('+');
-        allEquations.push(' +<br>');    
+        allEquations.push(' +<br>');
         DOMstrings.resultDisplay.textContent = '';
     }
-    
-    
-        document.getElementById("subtract_button").onclick = function() 
+
+
+        document.getElementById("subtract_button").onclick = function()
     {
+        scrollDisplay('-');
         resultContent.push('-');
-        allEquations.push(' -<br>')    
-            
-        if (DOMstrings.resultDisplay.textContent = ' ') 
+        allEquations.push(' -<br>')
+
+        if (DOMstrings.resultDisplay.textContent = ' ')
     {
-            DOMstrings.resultDisplay.textContent = '-'; 
-            console.log('-');        
-    } else 
-        
+            DOMstrings.resultDisplay.textContent = '-';
+            console.log('-');
+    } else
+
     {
         DOMstrings.resultDisplay.textContent = '';
     }
-            
-    } 
-        
-        document.getElementById("multiply_button").onclick = function() 
+
+    }
+
+        document.getElementById("multiply_button").onclick = function()
     {
+        scrollDisplay('x');
         resultContent.push('*');
         allEquations.push(' x<br>');
         DOMstrings.resultDisplay.textContent = '';
-    } 
-        
-        document.getElementById("divide_button").onclick = function() 
+    }
+
+        document.getElementById("divide_button").onclick = function()
     {
+        scrollDisplay('÷');
         resultContent.push('/');
         allEquations.push(' ÷<br>');
         DOMstrings.resultDisplay.textContent = '';
-    }  
-        
-        document.getElementById("percentage_button").onclick = function() 
+    }
+
+        document.getElementById("percentage_button").onclick = function()
         {
+            scrollDisplay('%');
             resultContent.push('/100');
             allEquations.push('% x<br>');
             DOMstrings.resultDisplay.textContent += '%';
-        } 
-        
-////////////////////////////////////////////////////////////////////////////////////////////////////     
-        
- 
-        
-///////////////////////////////////////////////////////////////////////////////////////////////////       
-        
-        document.getElementById("equal_button").onclick = function() 
-        {
-            resulting();
-        };
-  
+        }
 
-        DOMstrings.ACbutton.onclick = function() 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//// LOOPS /////        
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+        document.getElementById("equal_button").onclick = function()
         {
+            equalPressed = true;
+            resulting();
+            let final = resultContent.join('');
+            let finalResult = eval(final);
+            scrollDisplay('=');
+            scrollDisplay(finalResult)
+        };
+
+
+        DOMstrings.ACbutton.onclick = function()
+        {
+            scrollDisplay('All Cleared');
             DOMstrings.resultDisplay.textContent = '';
-            DOMstrings.scrollButton.style.display = 'none';
             resultContent = [];
         };
 
-        DOMstrings.scrollButton.onclick = function() 
-        {
-            let finalEquations = allEquations.join('');
-            document.getElementById("p").textContent = finalEquations;
-           // console.log(finalEquations);
-        }
-        
-        
